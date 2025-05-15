928d4d0f4e0061703b6edff8c91bc7f220893d1b
Simeon Goranov <sgoranov@users.noreply.github.com>
2025-05-15 10:48:45 +0300

    Rename project

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -12,11 +12,11 @@ composer install --no-scripts
 
 # Database setup
 until psql -c "\q"; do sleep 3; done
-echo "SELECT 'CREATE DATABASE \"php-identity-link-2fa-google\"' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '\"php-identity-link-2fa-google\"')\gexec" \
+echo "SELECT 'CREATE DATABASE \"php-identity-link-2fa\"' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '\"php-identity-link-2fa\"')\gexec" \
  | psql -v ON_ERROR_STOP=1
 php bin/console -e dev doctrine:migrations:migrate --no-interaction
 
-echo "SELECT 'CREATE DATABASE \"php-identity-link-2fa-google-test\"' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '\"php-identity-link-2fa-google-test\"')\gexec" \
+echo "SELECT 'CREATE DATABASE \"php-identity-link-2fa-test\"' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '\"php-identity-link-2fa-test\"')\gexec" \
  | psql -v ON_ERROR_STOP=1
 php bin/console -e test doctrine:migrations:migrate --no-interaction
 php bin/console -e test -n doctrine:fixtures:load
